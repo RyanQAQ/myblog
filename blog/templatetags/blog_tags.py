a89c7fc63fdc4ee351d683e4664d10826a0c4722
lyk0915 <1107487574@qq.com>
2020-11-05 10:47:51 +0800

    1.优化搜索 2.添加微信二维码

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
+import re
 from django import template
+from django.utils.safestring import mark_safe
 from ..models import Article, Category, Tag
 from django.db.models.aggregates import Count
 
@@ -21,3 +23,16 @@ def get_category_list():
 def get_article_count():
     """获取文章总数"""
     return Article.objects.count()
+
+
+@register.simple_tag
+def title_highlight(text, q):
+    '''自定义标题搜索词高亮函数，忽略大小写'''
+    if len(q) > 1:
+        try:
+            text = re.sub(q, lambda a: '<span class="highlighted">{}</span>'.format(a.group()),
+                          text, flags=re.IGNORECASE)
+            text = mark_safe(text)
+        except:
+            pass
+    return text
